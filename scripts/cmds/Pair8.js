691f61f6cf884fdb6bed96edbff33de0e09720ca
mdquotex568-pixel <mdquotex568@gmail.com>
2026-06-04 16:34:00 +0600

    Update Pair8.js

--- scripts/cmds/Pair8.js
+++ scripts/cmds/Pair8.js
@@ -13,7 +13,7 @@ module.exports = {
 		version: "1.0",
 		author: "ashik",
 		countDown: 10,
-		role: 0,
+		role: 2,
 		shortDescription: "Pair with mentioned user",
 		longDescription: "Generate couple image with mentioned user",
 		category: "love",
