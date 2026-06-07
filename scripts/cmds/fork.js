89967866ac49f1791fafbdb00c2a06c63dba60d1
mdquotex568-pixel <mdquotex568@gmail.com>
2026-06-07 14:12:04 +0600

    Create fork.js

---
+++ scripts/cmds/fork.js
@@ -0,0 +1,46 @@
+module.exports = {
+	config: {
+		name: "fork",
+		version: "1.0",
+		author: "ashik",
+		countDown: 5,
+		role: 0,
+		shortDescription: "Bot source code",
+		longDescription: "Show bot fork link",
+		category: "info"
+	},
+
+	onStart: async function ({ message }) {
+		const msg = `
+╔══════════════════╗
+      🌸 𝘼𝙎𝙃𝙄𝙆 𝘽𝙊𝙏 🌸
+╚══════════════════╝
+
+👑 Owner: Ashik
+
+✨ Welcome To My Goat Bot Repository
+
+📌 Features:
+➤ Fast Response
+➤ Auto Download System
+➤ Modern Commands
+➤ Easy To Deploy
+
+🔗 Fork Link:
+https://github.com/mdquotex568-pixel/Ashik-goat.git
+
+Setup video
+https://youtu.be/zp61jH98vSQ?si=vbYqOqGOUQye9Qhu
+
+Telegram channel must join
+https://t.me/ashikgod
+
+━━━━━━━━━━━━━━━
+💖 Thanks For Using My Bot
+🚀 Fork & Enjoy
+━━━━━━━━━━━━━━━
+`;
+
+		return message.reply(msg);
+	}
+};
